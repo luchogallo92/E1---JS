00d561a3860230785922375879ed163e747bd1f2
Luciano Gallo <102675296+luchogallo92@users.noreply.github.com>
2024-02-15 23:51:30 -0300

    1.0

---
+++ main.js
@@ -0,0 +1,49 @@
+
+//// Ejercicio 1
+// let num1 = 4  
+
+// function paroimpar (num1) {
+//     if (num1 % 2 === 0) {
+//         console.log("El numero es par")    
+//     }
+//     else {
+//         console.log ("El numero es impar")  
+//     }
+// }
+
+// //paroimpar (num1)
+
+//// Ejercicio 2
+
+//  let num1 = 4
+//  let num2 = 5
+
+//  function mayor_o_menor() {
+//      if (num1 > num2) {
+//          console.log("El número " + num1 + " es mayor que " + num2);
+//      } else if (num1 === num2) {
+//          console.log ("Los numeros ingresados son iguales");
+//      } else {
+//      console.log("El número " + num1 + " es menor que " + num2);
+//      }
+// }
+
+// mayor_o_menor();
+
+//Ejecicio 3
+
+// let num1 = 20
+
+// function multiplo (num1) {
+//     if (num1 % 5 === 0) {
+//         console.log ("El numero " + (num1) + " es multiplo de 5")
+//     }
+//     else {
+//         console.log ("El numero " + (num1) + " no es multiplo de 5")
+//     }
+
+// }
+
+// multiplo (num1);
+
+//Ejercicio 4
